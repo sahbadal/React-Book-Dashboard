9a85c232f5362fd63b819dbcec95fd0ed348bab8
badalsah <badalsah8541@gmail.com>
2025-02-19 16:56:56 +0530

    cors setup

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -12,6 +12,7 @@ app.use(
         origin: config.frontendDomain,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
+        credentials:true,
     })
 );
 
