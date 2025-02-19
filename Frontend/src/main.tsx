cd6a74d0c824025d65c28086d29ba88d5a8de1d1
badalsah <badalsah8541@gmail.com>
2025-02-19 14:22:04 +0530

    add tanstack query

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from '@/router.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './router.tsx';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </React.StrictMode>
+);
